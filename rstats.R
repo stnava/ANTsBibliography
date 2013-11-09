46e14fc116a212b8191381da965ca353f9a0b210
brian avants <stnava@gmail.com>
2013-11-09 12:28:26 -0500

    ENH: rstats example

---
+++ rstats.R
@@ -0,0 +1,3 @@
+
+ journals=c("Neuroimage","Brain","Brain and language","Neuropsychologia","Hippocampus","Neuroscience Letters","Neuroscience","Neuropsychology","Cortex","Neurobiology of aging","Neurology","Radiology","Neuropsychopharmacology","Medical Imaging, IEEE Transactions on","The Journal of Neuroscience","Magnetic Resonance in Medicine","Pattern Analysis and Machine Intelligence, IEEE Transactions on")
+ print( get_num_top_journals(id, journals) )
