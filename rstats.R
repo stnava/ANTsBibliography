1450edc3cc303aff9a21dd534817ced39195408a
BBAvants <stnava@gmail.com>
2014-10-27 09:19:35 -0400

    ENH: use scholar tools

--- rstats.R
+++ rstats.R
@@ -1,3 +1,10 @@
-
+library(scholar) # https://github.com/jkeirstead/scholar
+library(visreg)
+id<-"ox-mhOkAAAAJ"
+print( get_profile(id) )
+citehist<-get_citation_history(id)
+print(citehist)
+mdl<-lm( cites ~ stats::poly(year,4)  , data=citehist )
+visreg(mdl)
  journals=c("Neuroimage","Brain","Brain and language","Neuropsychologia","Hippocampus","Neuroscience Letters","Neuroscience","Neuropsychology","Cortex","Neurobiology of aging","Neurology","Radiology","Neuropsychopharmacology","Medical Imaging, IEEE Transactions on","The Journal of Neuroscience","Magnetic Resonance in Medicine","Pattern Analysis and Machine Intelligence, IEEE Transactions on")
  print( get_num_top_journals(id, journals) )
